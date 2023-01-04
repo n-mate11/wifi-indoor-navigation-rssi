ec05ebe14bccd66710543503c6e63642c0768ce8
Máté Nagy <nagymatepeter@student.vu.nl>
2023-01-04 12:23:54 +0100

    Create CSV file with wifi and coordinate data

--- app/src/main/java/com/example/thesisandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/thesisandroidapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.thesisandroidapp
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -10,29 +11,78 @@ import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import kotlin.collections.ArrayList
-import kotlin.math.pow
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
     private val arrayList: ArrayList<String> = ArrayList()
     private var adapter: ArrayAdapter<*>? = null
     private var listView: ListView? = null
+    private val numMeasurements = 10
+    private var x = 0
+    private var y = 0
 
+    private val AP04333 = ""
+    private val AP04096 = "5c:5a:c7:85:5c:2f"
+    private val AP04097 = "5c:5a:c7:66:46:8f" // not sure yet freq
+    private val AP04098 = ""
+    private val AP04099 = "5c:5a:c7:85:69:00" // not sure yet freq
+    private val AP04100 = "5c:5a:c7:72:a9:0f" // not sure yet freq
+
+    var writer: FileWriter? = null
+
+    @SuppressLint("SetTextI18n", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val startScan = findViewById<Button>(R.id.scanButton)
+        val startScan = findViewById<FloatingActionButton>(R.id.scanButton)
+
+        val incX = findViewById<ExtendedFloatingActionButton>(R.id.plusXButton)
+        val decX = findViewById<ExtendedFloatingActionButton>(R.id.minusXButton)
+        val incY = findViewById<ExtendedFloatingActionButton>(R.id.plusYButton)
+        val decY = findViewById<ExtendedFloatingActionButton>(R.id.minusYButton)
+
+        val xText = findViewById<TextView>(R.id.XCoord)
+        val yText = findViewById<TextView>(R.id.YCoord)
+
+        xText.text = "X $x"
+        yText.text = "Y $y"
+
         listView = findViewById(R.id.wifiList)
 
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList)
-        listView!!.setAdapter(adapter)
+        listView!!.adapter = adapter
+
+        incX.setOnClickListener {
+            x += 1
+            xText.text = "X $x"
+        }
+
+        decX.setOnClickListener {
+            x -= 1
+            xText.text = "X $x"
+        }
+
+        incY.setOnClickListener {
+            y += 1
+            yText.text = "Y $y"
+        }
+
+        decY.setOnClickListener {
+            y -= 1
+            yText.text = "Y $y"
+        }
 
         startScan.setOnClickListener {
             onStartScan()
@@ -43,26 +93,35 @@ class MainActivity : AppCompatActivity() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val wifiMan = context!!.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
             val scanResults = wifiMan.scanResults
-            var resultText = ""
+            var resultText: String
             Log.i("scan", scanResults.toString())
+
+            Log.d("File", "Writing to " + getStorageDir())
+            try {
+                writer = FileWriter(File(getStorageDir(),"scan.csv"), true
+                )
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
             for (scanResult in scanResults) {
-                if (scanResult.SSID == "VU-Campusnet") {
+                //if (scanResult.SSID == "VU-Campusnet" && (scanResult.BSSID == AP04096 || scanResult.BSSID == AP04097 || scanResult.BSSID == AP04099)) {
+                if (scanResult.SSID == "Mezga5" || scanResult.SSID == "Mezga1" || scanResult.SSID == "Geza") {
                     resultText = "SSID: " + scanResult.SSID + "\n" +
-                                 "BSSID: " + scanResult.BSSID + "\n" +
-                                 "frequency: " + scanResult.frequency + "\n" +
-                                 "level: " + scanResult.level + "\n" +
-                                 "distance: " + calculateDistanceRSSI(scanResult.level) + "\n"
+                            "BSSID: " + scanResult.BSSID + "\n" +
+                            "frequency: " + scanResult.frequency + "\n" +
+                            "level: " + scanResult.level + "\n" +
+                            "Coordinates: $x, $y"
+
                     arrayList.add(resultText)
                     adapter!!.notifyDataSetChanged()
+
+                    val data = "${scanResult.level},$x,$y,0\n"
+                    writer?.write(data)
+
                 }
             }
         }
-    }
-
-    private fun calculateDistanceRSSI(RSSILevel: Int): Double {
-        val n = 2.7
-        val distance = 10.0.pow(((53 - RSSILevel) / 10 * n))
-        return distance
     }
 
     private fun checkIfHasPermission(permission: String): Boolean {
@@ -74,7 +133,7 @@ class MainActivity : AppCompatActivity() {
         val wifiMan = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         if (!wifiMan.isScanAlwaysAvailable && !wifiMan.isWifiEnabled) {
             Log.i("wifi", "wifi is not enabled")
-            wifiMan.setWifiEnabled(true)
+            wifiMan.isWifiEnabled = true
             return
         }
 
@@ -107,6 +166,7 @@ class MainActivity : AppCompatActivity() {
             Log.i("scan accessWifi",  accessWifi.toString())
             Log.i("scan changeWifi",  changeWifi.toString())
             Log.i("scan accessNetwork",  accessNetwork.toString())
+
             doStartScan()
         }
     }
@@ -140,9 +200,13 @@ class MainActivity : AppCompatActivity() {
         // Make sure we unregister our receiver to avoid memory leaks.
         try {
             applicationContext.unregisterReceiver(wifiScanReceiver)
+            writer?.close()
         } catch (e: Exception) {
             Log.i("scan","Wi-Fi scan results receiver already unregistered")
         }
     }
 
+    private fun getStorageDir(): String? {
+        return getExternalFilesDir(null)!!.absolutePath
+    }
 }
